93945f92747e7ef8bc8d244262610569cf57213d
Eslam10497 <eslam.wael104@gmail.com>
2022-11-18 01:44:59 +0200

    feat: add react router setup and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
+import Login from "./components/Login";
+import Blog from "./components/Blog";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/blogs/:blogID",
+        element: <Blog />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
