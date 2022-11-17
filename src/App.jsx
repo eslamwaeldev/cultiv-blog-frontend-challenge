2ae30e44672bd9808f2375ceecc6b3ea908e6600
Eslam10497 <eslam.wael104@gmail.com>
2022-11-18 01:44:11 +0200

    feat: add navbar and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
 
-function App() {
+function App(props) {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div className="h-screen w-full">
+        <Navbar />
+        <div id="detail">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
