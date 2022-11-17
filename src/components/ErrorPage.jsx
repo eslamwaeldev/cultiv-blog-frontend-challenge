5f6c11893f559b22057fa00ac891f3c1c60b9d05
Eslam10497 <eslam.wael104@gmail.com>
2022-11-18 01:45:41 +0200

    feat: add error page component

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page" className="w-screen h-full flex justify-center items-center flex-col gap-7">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
