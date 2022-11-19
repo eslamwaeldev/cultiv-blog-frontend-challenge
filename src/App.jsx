9bd8145edd1340f7be3a5503da8b7cb73b9e91cf
Eslam10497 <eslam.wael104@gmail.com>
2022-11-19 06:55:06 +0200

    feat: remove unused state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="h-screen sm:w-full w-screen bg-gray-200">
         <Navbar />
-        <div id="detail">
-          <Outlet />
-        </div>
+        <Outlet />
       </div>
     </>
   );
