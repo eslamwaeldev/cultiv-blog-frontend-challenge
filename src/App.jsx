35017df991c32e6c0172a362df9333cd932104a7
Eslam10497 <eslam.wael104@gmail.com>
2022-11-20 22:21:45 +0200

    fix: fix a horizontal scroll bug

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 function App(props) {
   return (
     <>
-      <div className="h-screen sm:w-full w-screen bg-gray-200">
+      <div className=" flex flex-col h-screen  sm:min-w-full  sm:w-full w-screen bg-gray-200">
         <Navbar />
         <Outlet />
       </div>
