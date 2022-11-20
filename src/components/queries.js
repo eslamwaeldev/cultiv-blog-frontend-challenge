e23505c39e899c508a264f9087c87a9982feb810
Eslam10497 <eslam.wael104@gmail.com>
2022-11-20 03:45:21 +0200

    feat: add find one post query

--- src/components/queries.js
+++ src/components/queries.js
@@ -3,6 +3,20 @@ import { gql } from "@apollo/client";
 export const GET_POSTS = gql`
   query allPosts {
     allPosts {
+      id
+      title
+      author
+      content
+      next
+      prev
+      date
+    }
+  }
+`;
+
+export const GET_POST = gql`
+  query Post($id: ID!) {
+    Post(id: $id) {
       title
       author
       content
